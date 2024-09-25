1a1e1769168d7e30c14c96195d70817d500c0483
felipe <felipe.baleche2@gmail.com>
2024-09-25 09:27:13 -0300

    Implement word count Reducer

--- src/main/java/pr/puc/mapreduce/WCReducer.java
+++ src/main/java/pr/puc/mapreduce/WCReducer.java
@@ -1,11 +1,21 @@
 package pr.puc.mapreduce;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
 
 public class WCReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-  protected void reducer(Text key, Iterable<IntWritable> values, Context context) {
+  protected void reducer(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+    int occurrances = 0;
+
+    for (IntWritable value : values) {
+      occurrances += value.get();
+    }
+
+    context.write(key, new IntWritable(occurrances));
   }
 }
